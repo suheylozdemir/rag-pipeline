93361ef2eee2c71c87c53c7dbb70a4787c71bc2e
suheylozdemir <suheylozdemir@icloud.com>
2026-04-25 06:59:43 +0300

    Fix: use langchain_text_splitters module

--- app/rag.py
+++ app/rag.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
